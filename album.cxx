4f8cc469b50036a2e4546573450ca7542eb02f02
Alexander Logumanov <aminought@gmail.com>
2018-03-22 21:50:34 +0500

    Extract album title translation

--- album.cxx
+++ album.cxx
@@ -13,7 +13,7 @@ Title -> Word<h-reg1, l-quoted, ~r-quoted> Word<~l-quoted, r-quoted>;
 Title -> Word<h-reg1, l-quoted, ~r-quoted> AnyWord<~r-quoted>+ Word<~l-quoted, r-quoted>;
 
 TitleWQ -> Word<h-reg1> Word<h-reg1>*;
-TitleWQ -> Word<h-reg1>+ Word Word<h-reg1>;
+TitleWQ -> Word<h-reg1>+ AnyWord Word<h-reg1>;
 
 // OriginalTitle -> Word Hyphen* LBracket Word* RBracket Hyphen* Word interp (Album.OriginalName);
 
@@ -22,21 +22,21 @@ InBrackets -> LBracket Word* RBracket;
 AlbumTitle -> Title<fw> interp(Album.Name::not_norm);
 AlbumTitle -> TitleWQ<fw> interp(Album.Name::not_norm);
 
-//Premiere -> "выйти" | "выпустить" | "издать" "в" | "записать";
-//PostW -> "год"<gram="род"> ;//| "экземпляр";
-//S -> Premiere (Word) Date interp (Album.Date::not_norm);
+Premiere -> "выйти" | "выпустить" | "издать" "в" | "записать" | "состояться";
+PostW -> "год"<gram="род"> ;//| "экземпляр";
+S -> Premiere (Word) Date interp (Album.Date::not_norm);
 
 Name -> Word<h-reg1, ~fw> Word<h-reg1>*; 
 SingerPref -> 'альбом' | 'группа'<gram="род"> | 'музыкант'<gram="род"> | 'певица'<gram="род"> | 'рэпер'<gram="род">;
 Singer -> SingerPref NP* Name interp (Album.Singer);
 
-//Lang -> "англ" | "рус" | "англ." | "рус.";
-//Translate -> LBracket Word* Lang Hyphen* Title interp (Album.TranslateName::not_norm) RBracket;
+TranslateTitle -> Title | TitleWQ | Word<h-reg1>+ AnyWord;
+Lang -> "англ" | "рус" | "англ." | "рус.";
+Translate -> LBracket Word* Lang Hyphen* TranslateTitle interp (Album.TranslateName::not_norm) RBracket;
 
 //S -> AlbumTitle Hyphen* (Date interp (Album.Date::not_norm)) Singer;
 //S -> AlbumTitle (Singer);
 //S -> AlbumTitle Hyphen* Word* Singer;
 //S -> AlbumTitle InBrackets Hyphen* Word* Singer;
 //S -> Singer;
-S -> AlbumTitle Word* Hyphen AnyWord* Singer;
-S -> AlbumTitle LBracket AnyWord* (Hyphen) (AnyWord*) RBracket Hyphen AnyWord* Singer;
+S -> AlbumTitle (Translate) Hyphen AnyWord* Singer;
